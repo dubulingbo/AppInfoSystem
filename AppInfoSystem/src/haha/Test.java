b8a0d297023107f16ca8902807a58083180d4893
dubulingbo <1379159810@qq.com>
2019-03-05 18:06:41 +0800

    添加了测试类，为了验证Git的使用

---
+++ AppInfoSystem/src/haha/Test.java
@@ -0,0 +1,12 @@
+package haha;
+
+public class Test {
+	public void test01(){
+		System.out.println("hello,New World!");
+	}
+	
+	public static void main(String[] args) {
+		Test test = new Test();
+		test.test01();
+	}
+}
